c21135f46fce45148a7b12d50a680f30405e1b65
Mark Wolgin <wolginmark@gmail.com>
2024-03-17 09:49:18 -0400

    Status

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,2 +1,4 @@
-find jenkins-* -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd {} && pwd && \
+find jenkins-* -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cp ./scripts ./{}/scripts -r && \
+  cd {} && \
+  pwd && \
   docker build -t wolginm/{}:latest ." \;
